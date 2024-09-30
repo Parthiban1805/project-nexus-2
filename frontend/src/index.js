92e69a43057623c2d01fe5bb58165163d3e44426
Parthiban <parthi.s1805@gmail.com>
2024-09-30 21:39:33 +0530

    init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import StoreContextProvider from './context/StoreContext'; // Ensure the path is correct
+import StoreContextProvider from './context/StoreContext'; 
 
-// Create a root for the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the App wrapped in StoreContextProvider
 root.render(
   <StoreContextProvider>
     <App />
